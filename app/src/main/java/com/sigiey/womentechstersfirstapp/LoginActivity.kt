91aa5b3f2c73ff044130237da7b8b8c72672423c
ndush <mdamarisnduku@gmail.com>
2023-11-25 02:15:37 +0300

    update

--- app/src/main/java/com/sigiey/womentechstersfirstapp/LoginActivity.kt
+++ app/src/main/java/com/sigiey/womentechstersfirstapp/LoginActivity.kt
@@ -1,11 +1,35 @@
-package com.sigiey.womentechstersfirstapp
+package com.wtm.womentechstersfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var createNAButton: Button = findViewById(R.id.createNewAccount)
+        createNAButton.setOnClickListener {
+            var i = Intent(this, CreateNewAccountActivity::class.java)
+            startActivity(i)
+        }
+
+        val forgotPasswordBtn: Button = findViewById(R.id.forgottenPassword)
+        forgotPasswordBtn.setOnClickListener {
+            var i = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(i)
+        }
+
+        val loginBtn: Button = findViewById(R.id.loginButton)
+        loginBtn.setOnClickListener{
+            navigateToLoginPage()
+        }
+    }
+
+    private fun navigateToLoginPage() {
+        var intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
