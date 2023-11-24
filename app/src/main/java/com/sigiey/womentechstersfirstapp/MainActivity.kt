ed54dd086e07f422619340e994bb72b6ee4cc868
ndush <mdamarisnduku@gmail.com>
2023-11-25 01:08:43 +0300

    Welocome back Activity

--- app/src/main/java/com/sigiey/womentechstersfirstapp/MainActivity.kt
+++ app/src/main/java/com/sigiey/womentechstersfirstapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.sigiey.womentechstersfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private fun startActivity(intent: MainActivity) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var joinButton: Button = this.findViewById(R.id.joinNowButton)
+        joinButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
