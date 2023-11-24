183c8eac15bbd8d33b995d4c879eea8c8ea260c5
ndush <mdamarisnduku@gmail.com>
2023-11-25 02:22:03 +0300

    update

--- app/src/main/java/com/sigiey/womentechstersfirstapp/HomeActivity.kt
+++ app/src/main/java/com/sigiey/womentechstersfirstapp/HomeActivity.kt
@@ -2,35 +2,10 @@ package com.wtm.womentechstersfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import com.google.android.material.snackbar.Snackbar
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
-
-        val sendBtn: Button = findViewById(R.id.send)
-        val emailEdt: EditText = findViewById(R.id.email)
-
-        sendBtn.setOnClickListener {
-            val userEmailEntered: String = emailEdt.text.toString()
-            var message = "";
-
-            if(!userEmailEntered.matches(Regex("^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\\.[a-zA-Z]+"))){
-                message = "You need to enter an email address"
-            }else{
-                message = "A recovery email has been sent to $userEmailEntered"
-            }
-
-            Snackbar
-                .make(
-                    findViewById(R.id.layout),
-                    message,
-                    Snackbar.LENGTH_LONG
-                )
-                .show()
-        }
+        setContentView(R.layout.activity_home)
     }
 }
